ed29fe9b747e8dc03c09b051df79b44cd6064b56
hansini <hansinichikhale>
2019-09-14 17:39:59 +0530

    maked changes in Account page java

--- src/test/java/crm/qa/testcases/AccountPageTest.java
+++ src/test/java/crm/qa/testcases/AccountPageTest.java
@@ -1,7 +1,12 @@
 package crm.qa.testcases;
 
+import org.testng.annotations.Test;
+
 import crm.testbase.TestBase;
 
 public class AccountPageTest extends TestBase{
-
+@Test
+public void log() {
+	System.out.println("hello");
+} 
 }
